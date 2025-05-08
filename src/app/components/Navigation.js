2da63e1c11ee2bc6a6a36409d988d4a11fb2107f
EKomin220 <ifa.komin@gmail.com>
2025-05-08 13:53:35 +0100

    added links to navbar

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
 
 const Navigation = () => {
   return (
-    <div>Navigation</div>
-  )
-}
+    <nav className="flex items-center justify-between flex-wrap bg-sky-800 p-10">
+      <div>
+        <div>
+          <Link href="/">View noticeboard</Link>
+        </div>
+        <div>
+          <Link href="/newpost">Pin a new post</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
